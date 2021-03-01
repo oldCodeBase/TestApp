5351ba16aff9b91ab54c28be5d4e6c2b6c623891
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-03-01 13:24:44 +0300

    Created custom SecondaryLabel

---
+++ weather/Custom Views/SecondaryLabel.swift
@@ -0,0 +1,39 @@
+//
+//  SecondaryLabel.swift
+//  weather
+//
+//  Created by Ibragim Akaev on 28/02/2021.
+//
+
+import UIKit
+
+class SecondaryLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    convenience init(textAlignment: NSTextAlignment) {
+        self.init(frame: .zero)
+        self.textAlignment = textAlignment
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        textColor                                   = .secondaryLabel
+        font                                        = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontForContentSizeCategory           = true
+        adjustsFontSizeToFitWidth                   = true
+        minimumScaleFactor                          = 0.75
+        numberOfLines                               = 1
+        translatesAutoresizingMaskIntoConstraints   = false
+    }
+    
+}
